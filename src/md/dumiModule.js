aebe87ff52ebd97b55b816a1dfa18ffed999ce37
Deuscx <48537979+Deuscx@users.noreply.github.com>
2021-01-25 19:43:40 +0800

    :bug: fix build script

--- src/md/dumiModule.js
+++ src/md/dumiModule.js
@@ -14,7 +14,7 @@ module.exports =  class DumiMd extends BaseMd{
 
     async beforeGenerate(){
        fs.ensureDirSync(this.docsRootDir);
-       await executeCommand('yarn',["create","@umijs/dumi-lib"], this.docsRootDir );
+       await executeCommand('yarn',["create","@umijs/dumi-app"], this.docsRootDir );
        fs.emptyDirSync( this.docsDir);
     }
 
